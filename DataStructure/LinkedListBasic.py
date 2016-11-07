59516acfc32342d978b4f021ad3c0dd57f5eec56
Mahesh Masale <masale123@hotmail.com>
2016-11-06 21:39:08 -0500

    learning increased

--- DataStructure/LinkedListBasic.py
+++ DataStructure/LinkedListBasic.py
@@ -1,19 +1,18 @@
 #https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list
 
-"""
- Print elements of a linked list on console
- head input could be None as well for empty list
- Node is defined as
+class Node(object):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
 
- class Node(object):
-
-   def __init__(self, data=None, next_node=None):
-       self.data = data
-       self.next = next_node
-
-
-"""
-
+def createLinkedList(arr):
+    s = Node(arr[0])
+    head = s
+    for i in range(1,len(arr)):
+        temp = Node(arr[i])
+        s.next = temp
+        s = temp
+    return head
 
 def print_list(head):
     NodeN = head
@@ -22,3 +21,8 @@ def print_list(head):
     while NodeN.next != None:
         NodeN = NodeN.next
         print(NodeN.data)
+
+newHead = createLinkedList([1,2,3])
+while newHead != None:
+    print(newHead.data)
+    newHead = newHead.next
