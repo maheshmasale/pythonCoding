cd8f3e3d20413e38643abf90c8ad9a3173d09ef0
Mahesh Masale <masale123@hotmail.com>
2016-11-06 22:19:14 -0500

    learning increased

---
+++ DataStructure/bracketMatchinWithStack.py
@@ -0,0 +1,40 @@
+class stack():
+    def __init__(self):
+        self.arr = []
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def size(self):
+        return len(self.arr)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.arr.pop()
+        else:
+            return None
+
+    def push(self,val):
+            self.arr.append(val)
+
+    def peek(self):
+        return self.arr[self.size() -1]
+
+s = "{{[[(())]]}}"
+def bracketMatching(str):
+    s = stack()
+    for i in str:
+        if s.size() ==0:
+            s.push(i)
+        else:
+            if s.peek() == i:
+                s.pop()
+            else:
+                s.push(i)
+
+    return s.size() == 0
+
+if bracketMatching(s):
+    print("YES")
+else:
+    print("NO")
